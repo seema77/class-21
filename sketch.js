010be433b7bf09bded6eb9b02097ab08af709561
seema77 <76130253+seema77@users.noreply.github.com>
2023-01-05 03:00:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball,groundObj,leftSide,rightSide;
+var world;
+var radius = 40;
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	var ball_options={
+		isStatic:false,
+		restitution:0.3,
+		friction:0,
+		density:1.2
+	}
+
+	//create ball body
+
+	//create 3 objects
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+
+  ellipse(ball.position.x,ball.position.y,radius,radius);
+
+  groundObj.display();
+  leftSide.display();  
+  rightSide.display();
+  
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+		//apply force
+    
+  	}
+}
